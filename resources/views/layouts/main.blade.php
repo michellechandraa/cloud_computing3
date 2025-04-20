26a8af23d9cd9b3add1f01db066aba22d03264a1
Michelle Chandra <74892252+michellechandraa@users.noreply.github.com>
2025-04-20 14:24:25 +0700

    Front-end ganti warna bg dan warna primary

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -8,7 +8,7 @@
     {{-- <link rel="stylesheet" href="/css/app.css"> --}}
     @vite('resources/css/app.css')
 </head>
-<body class="font-sans bg-gray-900 text-white">
+<body class="font-sans bg-black text-white">
     <nav class="border-b border-gray-800">
         <div class="container mx-auto px-4 flex items-center justify-between px-4 py-6">
             <ul class="flex items center">
@@ -18,18 +18,15 @@
                     </a> 
                 </li>
                 <li class="ml-16">
-                    <a href="#" class="hover:text-gray-300">Movies</a>
+                    <a href="http://127.0.0.1:8000" class="hover:text-gray-300">Movies</a>
                 </li>
                 <li class="ml-6">
                     <a href="#" class="hover:text-gray-300">TV Shows</a>
                 </li>
-                <li class="ml-6">
-                    <a href="#" class="hover:text-gray-300">Actors</a>
-                </li>
             </ul>
             <div class="flex items-center">
                 <div class="relatives">
-                    <input type="text" class="bg-gray-800 text-sm rounded-full w-64 px-4 pl-8 py-1" placeholder="Search">
+                    <input type="text" class="bg-gray-900 text-sm rounded-full w-64 px-4 pl-8 py-1" placeholder="Search">
                     <div class="absolute top-0">
                         <svg class="fill-current w-4 text-gray-500 mt-8 ml-2" viewBox="0 0 24 24"><path class="heroicon-ui" d="M16.32 14.9l5.39 5.4a1 1 0 01-1.42 1.4l-5.38-5.38a8 8 0 111.41-1.41zM10 16a6 6 0 100-12 6 6 0 000 12z"/></svg>
                     </div>
